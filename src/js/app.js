5daf7d1536a713e5a756cbeaad94c28e04dff345
wgimson <wgimson@mmm.com>
2018-06-05 11:42:33 -0400

    leftovers

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,17 @@ App = {
 
   init: function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    $.getJSON("../pets.json", function(data) {
+      var petsRow = $("#petsRow");
+      var petTemplate = $("#petTemplate");
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+      for (i = 0; i < data.length; i++) {
+        petTemplate.find(".panel-title").text(data[i].name);
+        petTemplate.find("img").attr("src", data[i].picture);
+        petTemplate.find(".pet-breed").text(data[i].breed);
+        petTemplate.find(".pet-age").text(data[i].age);
+        petTemplate.find(".pet-location").text(data[i].location);
+        petTemplate.find(".btn-adopt").attr("data-id", data[i].id);
 
         petsRow.append(petTemplate.html());
       }
@@ -24,41 +24,93 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+    // Is there an injected web3 instance?
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON("Tracking.json", function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var TrackingArtifact = data;
+      App.contracts.Tracking = TruffleContract(TrackingArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Tracking.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on("click", ".btn-adopt", App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  markTracked: function(sources, account) {
+    var sourceInstance;
+
+    App.contracts.Tracking.deployed()
+      .then(function(instance) {
+        sourceInstance = instance;
+
+        return sourceInstance.getSources.call();
+      })
+      .then(function(sources) {
+        for (i = 0; i < sources.length; i++) {
+          if (sources[i] !== "0x0000000000000000000000000000000000000000") {
+            $(".panel-pet")
+              .eq(i)
+              .find("button")
+              .text("Success")
+              .attr("disabled", true);
+          }
+        }
+      })
+      .catch(function(err) {
+        console.log(err.message);
+      });
   },
 
-  handleAdopt: function(event) {
+  handleTrack: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var seedId = parseInt($(event.target).data("id"));
 
-    /*
-     * Replace me...
-     */
+    var sourceInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Tracking.deployed()
+        .then(function(instance) {
+          sourceInstance = instance;
+
+          // Execute adopt as a transaction by sending account
+          return sourceInstance.adopt(seedId, { from: account });
+        })
+        .then(function(result) {
+          return App.markTracked();
+        })
+        .catch(function(err) {
+          console.log(err.message);
+        });
+    });
   }
-
 };
 
 $(function() {
